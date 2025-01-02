5e7e8c6e30b994e9fb25fe990aa2ef2da535e832
Raboonisherin <112843929+Raboonisherin@users.noreply.github.com>
2025-01-02 20:41:58 +0530

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    prices: {
+      type: [Number],
+      required: true,
+    },
+    extraOptions: {
+      type: [
+        {
+          text: { type: String, required: true },
+          price: { type: Number, required: true },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+let Product;
+
+try {
+  // Try to retrieve the existing Product model if it exists
+  Product = mongoose.model('Product');
+} catch {
+  // If the model doesn't exist, define it
+  Product = mongoose.model('Product', ProductSchema);
+}
+
+export default Product;
